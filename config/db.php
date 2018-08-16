86c777860f881c450a71d2829ea06a00a00d59e8
Enrico Piva <enrico88@gmail.com>
2018-08-16 23:36:51 +0200

    installato rewrite index

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=changexpress',
     'username' => 'root',
-    'password' => '3pasquaman3',
+    'password' => 'gigi2013',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
