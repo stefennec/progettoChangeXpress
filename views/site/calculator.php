acad1d265c2bf30c2391c8cd9aa4641791f5cb20
stefennec <pasquaman3@gmail.com>
2018-08-19 17:09:44 +0200

    calcolatrice funzionante

--- views/site/calculator.php
+++ views/site/calculator.php
@@ -2,22 +2,25 @@
       <div class="row">
         <div class="col-md-6 mx-auto">
           <div class="card card-body text-center mt-5">
-            <h1 class="heading display-5 pb-3">Loan Calculator</h1>
+            <h1 class="heading display-5 pb-3">Acquisto Valute</h1>
             <form id="loan-form">
               <div class="form-group">
                 <div class="input-group">
                   <span class="input-group-addon">$</span>
-                  <input type="number" class="form-control" id="amount" placeholder="Loan Amount">
+                  <input type="number" class="form-control" id="quantita" placeholder="quantitaValuta">
                 </div>
               </div>
               <div class="form-group">
                 <div class="input-group">
                   <span class="input-group-addon">%</span>
-                  <input type="number" class="form-control" id="interest" placeholder="Interest">
+                  <input type="number" class="form-control" id="rate" placeholder="Cambio Applicato">
                 </div>
               </div>
               <div class="form-group">
-                <input type="number" class="form-control" id="years" placeholder="Years To Repay">
+                <input type="number" class="form-control" id="commissione" placeholder="Commissione Applicata">
+              </div>
+              <div class="form-group">
+                <input type="number" class="form-control" id="spesa" placeholder="Spesa Fissa">
               </div>
               <div class="forn-group">
                 <input type="submit" value="Calculate" class="btn btn-dark btn-block">
@@ -26,7 +29,7 @@
             <!-- LOADER -->
 
             <div id="loading">
-              <img src="loading.gif" alt="">
+              <img src="/img/loading.gif" alt="">
             </div>
 
             <!-- RESULTS -->
@@ -34,22 +37,22 @@
               <h5>Results</h5>
               <div class="form-group">
                 <div class="input-group">
-                  <span class="input-group-addon">Monthly Payment</span>
-                  <input type="number" class="form-control" id="monthly-payment" disabled>
+                  <span class="input-group-addon">Netto al Cliente</span>
+                  <input type="number" class="form-control" id="netto-transazione" disabled>
                 </div>
               </div>
 
               <div class="form-group">
                 <div class="input-group">
-                  <span class="input-group-addon">Total Payment</span>
-                  <input type="number" class="form-control" id="total-payment" disabled>
+                  <span class="input-group-addon">Commissione in €</span>
+                  <input type="number" class="form-control" id="commissione-transazione" disabled>
                 </div>
               </div>
 
               <div class="form-group">
                 <div class="input-group">
-                  <span class="input-group-addon">Total Interest</span>
-                  <input type="number" class="form-control" id="total-interest" disabled>
+                  <span class="input-group-addon">Lordo</span>
+                  <input type="number" class="form-control" id="lordo-transazione" disabled>
                 </div>
               </div>
             </div>
@@ -57,3 +60,7 @@
         </div>
       </div>
     </div>
+
+    <script type="text/javascript">
+
+    </script>
