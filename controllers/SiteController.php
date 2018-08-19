25f25049fac3742ef4e45e7dead8a009a1afa44b
Enrico Piva <enrico88@gmail.com>
2018-08-19 18:35:23 +0200

    site clienti

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -125,4 +125,8 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+    public function actionClienti()
+    {
+        return $this->render('clienti');
+    }
 }
